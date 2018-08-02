e679f7a928a8abd83ec345cbbfe5b8b97d09a4d2
asif023 <40739306+asif023@users.noreply.github.com>
2018-08-02 11:47:56 +0530

    reverse program

---
+++ code/c-programs/day-20/REVERSE.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,d,e,f,g,h,i;
+printf("enter the five digit number");
+scanf("%d",&a);
+b=a%10;
+c=a/10;
+d=c%10;
+e=c/10;
+f=e%10;
+g=e/10;
+h=g%10;
+i=g/10;
+printf("%d%d%d%d%d",b,d,f,h,i);
+getch();
+}
